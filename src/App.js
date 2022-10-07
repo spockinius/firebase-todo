af0a62b85725b0d59b99fdfa028a8ca35fbadd66
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-10-07 08:50:14 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems = () => {
+    fetch('https://todolist-7b01f-default-rtdb.europe-west1.firebasedatabase.app/.json')
+    .then(response => response.json())
+    .then(data => setTodos(Object.values(data)))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" noWrap>
+            TodoList
+          </Typography>
+        </Toolbar>
+      </AppBar> 
+      <div className="ag-theme-material" style={ { height: 400, width: 600, margin: 'auto' } }>
+        <AgGridReact rowData={todos}>
+          <AgGridColumn sortable={true} filter={true} field='description' />
+          <AgGridColumn sortable={true} filter={true} field='date' />
+          <AgGridColumn sortable={true} filter={true} field='priority' />
+          </AgGridReact>
+       </div>
     </div>
   );
 }
